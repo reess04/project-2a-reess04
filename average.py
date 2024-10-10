93e98d4d4cec74af9d8ae41d74caba19680d9d3c
reess04 <reess@oregonstate.edu>
2024-10-09 19:12:34 -0700

    Create average.py

---
+++ average.py
@@ -0,0 +1,10 @@
+print("Please enter five numbers.")
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+num5 = float(input())
+total = num1 + num2 + num3 + num4 + num5
+average = total / 5
+print("The average of those numbers is.")
+print(average)
