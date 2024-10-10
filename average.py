981d4a6c6dc1639a557ca593670a917e0ef44883
reess04 <reess@oregonstate.edu>
2024-10-09 19:15:25 -0700

    Update average.py

--- average.py
+++ average.py
@@ -6,5 +6,5 @@ num4 = float(input())
 num5 = float(input())
 total = num1 + num2 + num3 + num4 + num5
 average = total / 5
-print("The average of those numbers is.")
+print("The average of those numbers is:")
 print(average)
